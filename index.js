5dd0e35289faf831bf0c2d93c25aefd11ede20ff
Abderrahmane Mokaddem <1mokaddeme@gmail.com>
2025-10-17 22:30:49 +0100

    Add CORS for frontend domain

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"]
+    origin:["https://todo-travel-agency.netlify.app"]
 }))
 
 app.get("/destinations", async (req,res)=>{
